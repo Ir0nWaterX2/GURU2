be1153f757b0d75eb4fd6cbcc320ecbca806b4cf
intensec <intensec@naver.com>
2023-07-25 17:05:27 +0900

    lnputActivity이전버튼 추가

--- app/src/main/java/com/example/guru2/MainActivity2.kt
+++ app/src/main/java/com/example/guru2/MainActivity2.kt
@@ -1,11 +1,33 @@
 package com.example.guru2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity2 : AppCompatActivity() {
+
+    //InputActivity 이동 버튼
+    lateinit var btnToInputActivity : Button
+    //ListActivity 이동 버튼
+    lateinit var  btnToListActivity : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        btnToInputActivity = findViewById<Button>(R.id.btnToInputActivity)
+        btnToListActivity = findViewById<Button>(R.id.btnToListActivity)
+
+        //MainActivity에서 InputActivity로 이동 (기존 답변 수정)
+        btnToInputActivity.setOnClickListener {
+            var intent = Intent(this, InputActivity::class.java)
+            startActivity(intent) }
+
+        //MainActivity에서 ListActivity로 이동
+        btnToListActivity.setOnClickListener {
+            var intent = Intent(this,ListActivity ::class.java)
+            startActivity(intent)
+        }
     }
 }
