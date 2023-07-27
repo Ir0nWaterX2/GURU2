6ca79ca5ebe031f5586e8c5788c9917ef989ae2d
Ir0nWaterX2 <108861088+Ir0nWaterX2@users.noreply.github.com>
2023-07-27 23:52:43 +0900

    Revert "수정 후 입력 btn 누를 시 앱이 종료되는 오류 수정"

--- app/src/main/java/com/example/guru2/DBManager.kt
+++ app/src/main/java/com/example/guru2/DBManager.kt
@@ -12,10 +12,6 @@ class DBManager(
         version: Int
 ) : SQLiteOpenHelper(context, name, factory, version) {
 
-    init {
-        onCreate(writableDatabase)
-    }
-
     override fun onCreate(db: SQLiteDatabase?) {
 
     }
@@ -61,10 +57,6 @@ class DBManager(
 
     // 데이터 존재 여부 확인 함수
     fun isDataExist(year: Int, month: Int, day: Int): Boolean {
-        if (year <= 0 || month <= 0 || day <= 0) {
-            return false
-        }
-
         val db = readableDatabase
         val query = "SELECT * FROM userDB_$year WHERE month = $month AND day = $day"
         val cursor = db.rawQuery(query, null)
