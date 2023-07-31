c7152180a0229a96fadf2f657de01b5534755caf
intensec <intensec@naver.com>
2023-07-31 18:37:16 +0900

    InputActivity 타이틀 수정

--- app/src/main/java/com/example/guru2/InputActivity.kt
+++ app/src/main/java/com/example/guru2/InputActivity.kt
@@ -68,7 +68,7 @@ class InputActivity : AppCompatActivity() {
 
         // 현재 날짜에서 '일' 정보 가져오기
         val today: Int = calendar.get(Calendar.DAY_OF_MONTH)
-        tvTodayQuestion.text = "Day ${today}" + "    " + arrQuestion[today-1]
+        tvTodayQuestion.text = "Day ${today}\n" +   arrQuestion[today-1]
 
 
         // 뒤로가기 버튼
